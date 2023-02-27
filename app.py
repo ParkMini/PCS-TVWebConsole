1642baaa1f838c1e4fa2c3800597e416e8c8fce5
ParkMini <parkteammini@gmail.com>
2023-02-27 18:53:37 +0900

    콘솔 페이지 및 업로드 기능 완성

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ def main():
 def render():
     return render_template("render.html")
 
+@app.route("/console", methods=["GET"])
+def console():
+    return render_template("console.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    f = request.files['file']
+    f.save(secure_filename('video.mp4'))
+    return render_template("업로드가 완료되었습니다.")
+
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
